cd7e7070e26513d45ace295e5e3d2b8cab62f458
ttaranto <tom.taranto@yahoo.fr>
2021-06-13 22:13:24 +0200

    ajout d'un script spark shell

--- src/main/scala/spark_analysis.scala
+++ src/main/scala/spark_analysis.scala
@@ -2,5 +2,10 @@ import org.apache.spark.sql.SparkSession
 
 object spark_analysis extends App{
 
-  val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
+  val spark = SparkSession.builder.appName("Simple Application").master("local[*]")getOrCreate()
+  val df = spark.read.format("json").load("/home/ttaranto/hadoop_data/*.json")
+
+
+
+
 }
